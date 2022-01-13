2657a5e63c02ee8053df419b24b2354ec14f30b3
DaoVoTanIT <daovotan3@gmail.com>
2022-01-13 20:53:27 +0700

    docs: create CovidService class to get API

--- lib/home/data/service/covid_service.dart
+++ lib/home/data/service/covid_service.dart
@@ -1,23 +1,26 @@
 import 'dart:convert';
-import 'package:covidapp/home/data/model/Country.dart';
+import 'package:covidapp/home/data/model/global.dart';
 import 'package:http/http.dart' as http;
 import 'package:covidapp/home/data/model/country_summary.dart';
 import 'package:covidapp/home/data/model/county.dart';
 
 class CovidService {
-  // Future<GlobalSummaryModel> getGlobalSummary() async {
-  //   final data = await http.Client().get("https://api.covid19api.com/summary");
+  Future<GlobalSummaryModel> getGlobalSummary() async {
+    var url = Uri.parse('https://api.covid19api.com/summary');
+    final data = await http.Client().get(url);
 
-  //   if (data.statusCode != 200) throw Exception();
+    if (data.statusCode != 200) throw Exception();
 
-  //   GlobalSummaryModel summary =
-  //       new GlobalSummaryModel.fromJson(json.decode(data.body));
+    GlobalSummaryModel summary =
+        GlobalSummaryModel.fromJson(json.decode(data.body));
 
-  //   return summary;
-  // }
+    return summary;
+  }
+
   Future<List<CountrySummaryModel>> getCountrySummary(String slug) async {
     var url =
         Uri.parse('https://api.covid19api.com/total/dayone/country/' + slug);
+
     final data = await http.Client().get(url);
 
     if (data.statusCode != 200) throw Exception();
@@ -27,18 +30,6 @@ class CovidService {
         .toList();
     return summaryList;
   }
-
-  // Future<List<CountryModelData>> getCountrySummary() async {
-  //   var url = Uri.parse('https://corona.lmao.ninja/v2/countries');
-  //   final data = await http.Client().get(url);
-
-  //   if (data.statusCode != 200) throw Exception();
-
-  //   List<CountryModelData> summaryList = (json.decode(data.body) as List)
-  //       .map((item) => CountryModelData.fromJson(item))
-  //       .toList();
-  //   return summaryList;
-  // }
 
   Future<List<CountryModel>> getCountryList() async {
     var url = Uri.parse('https://api.covid19api.com/countries');
