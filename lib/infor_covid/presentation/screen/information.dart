57c7ebe8fb4bb0522a6cfea21b1b1a4d831b4b87
DaoVoTanIT <daovotan3@gmail.com>
2022-01-13 23:03:07 +0700

    feat: add prevention widget

--- lib/infor_covid/presentation/screen/information.dart
+++ lib/infor_covid/presentation/screen/information.dart
@@ -1,4 +1,5 @@
 import 'package:covidapp/home/presentation/widget/header_widget.dart';
+import 'package:covidapp/infor_covid/presentation/widget/prevention.dart';
 import 'package:covidapp/infor_covid/presentation/widget/symptom.dart';
 import 'package:flutter/material.dart';
 
@@ -95,53 +96,66 @@ class _InformationState extends State<Information> {
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Triệu chứng",
-                    style: kTitleTextstyle,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        SymptomWidget(
-                          image: "assets/images/headache.png",
-                          title: "Đau đầu",
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        SymptomWidget(
-                          image: "assets/images/high_fever.png",
-                          title: "Sốt cao",
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        SymptomWidget(
-                          image: "assets/images/caugh.png",
-                          title: "Ho",
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        SymptomWidget(
-                          image: "assets/images/headache.png",
-                          title: "Đau họng",
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 20),
+            //   child: Column(
+            //     crossAxisAlignment: CrossAxisAlignment.start,
+            //     children: [
+            //       Text(
+            //         "Triệu chứng",
+            //         style: kTitleTextstyle,
+            //       ),
+            //       SizedBox(
+            //         height: 15,
+            //       ),
+            //       SingleChildScrollView(
+            //         scrollDirection: Axis.horizontal,
+            //         child: Row(
+            //           children: [
+            //             PreventionWidget(
+            //                 image: "assets/images/wash_hands.png",
+            //                 title:
+            //                     "Rửa tay thường xuyên bằng xà phòng hoặc dung dịch sát khuẩn",
+            //                 text:
+            //                     "Rửa tay thường xuyên bằng xà phòng hoặc dung dịch sát khuẩn tay."),
+            //             PreventionWidget(
+            //                 image: "assets/images/face_mask.png",
+            //                 title: " khẩu trang ở nơi đông người",
+            //                 text: "Rửa tay kĩ"),
+            //             PreventionWidget(
+            //                 image: "assets/images/avoid_contact.png",
+            //                 title: "Giữ khoảng cách, hạn chế tiếp xúc",
+            //                 text: "Rửa tay kĩ"),
+            //             PreventionWidget(
+            //                 image: "assets/images/cover_cough.png",
+            //                 title: "Không đưa tay lên mặt",
+            //                 text: "Rửa tay kĩ"),
+            //           ],
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            SizedBox(height: 20),
+            Text("Prevention", style: kTitleTextstyle),
+            PreventionWidget(
+                image: "assets/images/wash_hands.png",
+                title: "Rửa tay",
+                text:
+                    "Rửa tay thường xuyên bằng xà phòng hoặc dung dịch sát khuẩn tay."),
+            PreventionWidget(
+                image: "assets/images/face_mask.png",
+                title: "khẩu trang",
+                text: "Rửa tay kĩ"),
+            PreventionWidget(
+                image: "assets/images/avoid_contact.png",
+                title: "Khoảng cách",
+                text: "Rửa tay kĩ"),
+            PreventionWidget(
+                image: "assets/images/cover_cough.png",
+                title: "Không đưa tay lên mặt",
+                text: "Rửa tay kĩ"),
+            SizedBox(height: 50),
           ],
         ),
       ),
