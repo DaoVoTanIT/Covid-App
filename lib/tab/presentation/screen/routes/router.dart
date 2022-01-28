4b103ff2657e36330c8fec9c7d7f39645b99b0c3
DaoVoTanIT <daovotan3@gmail.com>
2022-01-28 14:57:57 +0700

    refactor: change route page

--- lib/tab/presentation/screen/routes/router.dart
+++ lib/tab/presentation/screen/routes/router.dart
@@ -1,4 +1,3 @@
-import 'package:covidapp/country/presentation/screen/countryInfor.dart';
 import 'package:covidapp/country/presentation/screen/covid_country_detail.dart';
 import 'package:covidapp/infor_covid/presentation/screen/information.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +12,7 @@ class Routers {
       case 'information':
         return MaterialPageRoute(builder: (context) => Information());
       case 'country':
-        return MaterialPageRoute(builder: (context) => InforCovidCountry());
+        return MaterialPageRoute(builder: (context) => CovidCountryDetail());
       // case 'CurvedNavigationBarWidget':
       //   return MaterialPageRoute(
       //       builder: (context) => CurvedNavigationBarWidget());
