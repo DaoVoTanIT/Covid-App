f13d0a9866ded0cb7c2fd27c753bc0a5e65da9e6
DaoVoTanIT <daovotan3@gmail.com>
2022-03-25 10:19:19 +0700

    change route

--- lib/tab/presentation/screen/routes/router.dart
+++ lib/tab/presentation/screen/routes/router.dart
@@ -1,5 +1,7 @@
 import 'package:covidapp/country/presentation/screen/covid_country_detail.dart';
 import 'package:covidapp/infor_covid/presentation/screen/information.dart';
+import 'package:covidapp/readnews/presentation/screen/readnews.dart';
+import 'package:covidapp/splash/presentation/screen/splash.dart';
 import 'package:flutter/material.dart';
 
 import '../tab.dart';
@@ -7,23 +9,16 @@ import '../tab.dart';
 class Routers {
   static Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case 'splash':
+        return MaterialPageRoute(builder: (context) => IntroPage());
       case 'home':
         return MaterialPageRoute(builder: (context) => TabBarNavigation());
+      case 'readnews':
+        return MaterialPageRoute(builder: (context) => ReadNews());
       case 'information':
         return MaterialPageRoute(builder: (context) => Information());
       case 'country':
         return MaterialPageRoute(builder: (context) => CovidCountryDetail());
-      // case 'CurvedNavigationBarWidget':
-      //   return MaterialPageRoute(
-      //       builder: (context) => CurvedNavigationBarWidget());
-      // case 'HomePage':
-      //   return MaterialPageRoute(builder: (context) => RoomPage());
-      // case 'MapPage':
-      //   return MaterialPageRoute(builder: (context) => SearchMapPage());
-      // case 'SavedRoomPage':
-      //   return MaterialPageRoute(builder: (context) => FavoritePage());
-      // case 'ProfilePage':
-      //   return MaterialPageRoute(builder: (context) => ProfilePage());
       default:
         {
           return MaterialPageRoute(builder: (_) {
