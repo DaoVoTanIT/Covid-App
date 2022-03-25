45ec171bf1fe46f1977f05b66712bca770894f6b
DaoVoTanIT <daovotan3@gmail.com>
2022-03-25 10:19:35 +0700

    add route name

--- lib/tab/presentation/screen/routes/route_name.dart
+++ lib/tab/presentation/screen/routes/route_name.dart
@@ -1,5 +1,7 @@
 class RouteName {
+  static const String splash = 'splash';
   static const String home = 'home';
   static const String infor = 'information';
+  static const String readnews = 'readnews';
   static const String country = 'country';
 }
